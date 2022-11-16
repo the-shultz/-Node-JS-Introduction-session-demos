fc1da41c11dcf4794c03242841fcb3bfef53748a
Itay Cohen <itaycs2407@gmail.com>
2022-11-16 18:41:20 +0200

    demo 1

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
 console.log("this is the first print hello world!!!");
+
+const a = 5;
+let b = 56;
+b = 56.3565;
+
+const str = "this is a string";
+
+console.log(a, b);
+
+console.log({ a });
+
+console.log(b.toFixed(2));
+
+// console.log(Array.from(str).reverse());
+
+console.log(str.replaceAll(" is", " ****"));
+
+const bb = true;
+
+if (bb === true) {
+  console.log("bb is true");
+}

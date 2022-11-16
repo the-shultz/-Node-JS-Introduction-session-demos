a2894516962b98b09714a206bf0186d502bfa85a
Itay Cohen <itaycs2407@gmail.com>
2022-11-16 19:12:45 +0200

    demo3

---
+++ array.js
@@ -0,0 +1,26 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// const bb = arr.filter((value, index) => value % index === 0);
+
+// console.log(bb, arr);
+
+// arr[98] = 0;
+
+// console.log(arr);
+
+// const [b, n, ...v] = arr;
+
+// console.log(b, n, v);
+
+// con
+
+const a = () => console.log("i am a");
+const b = () => console.log("i am b");
+const c = () => console.log("i am c");
+const d = () => console.log("i am d");
+
+const functionArray = [a, b, c, d];
+
+functionArray.forEach((func) => func());
+
+console.log(functionArray);
---
+++ functions.js
@@ -0,0 +1,8 @@
+function calc(el1, el2) {
+  return el1 + el2;
+}
+
+console.log(calc(5, undefined));
+
+const b = 5;
+// const c = b + "";
